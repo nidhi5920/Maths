0a0b7f6f4a6ea07d73d32414e40e66021b718aa6
nidhi5920 <nidhisharma9126@gmail.com>
2023-10-12 12:48:28 +0530

    first commit

---
+++ Maths.xcframework/ios-arm64/Maths.framework/Headers/Maths.h
@@ -0,0 +1,18 @@
+//
+//  Maths.h
+//  Maths
+//
+//  Created by Nidhi Sharma on 12/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Maths.
+FOUNDATION_EXPORT double MathsVersionNumber;
+
+//! Project version string for Maths.
+FOUNDATION_EXPORT const unsigned char MathsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Maths/PublicHeader.h>
+
+
